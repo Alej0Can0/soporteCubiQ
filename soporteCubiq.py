407534ed7285a6c78a59f372fa730927a281e8b2
Alejandro Cano <caosofin@gmail.com>
2022-04-10 13:10:28 -0500

    Inicio de proceso modular

---
+++ soporteCubiq.py
@@ -0,0 +1,13 @@
+## Esta es el desarrollo de una aplicación (en python) funcional para la conexión y atención de soporte ágil y personalizada, con una conexión a base de datos desarrollada en mongoDB y un servicio dns con dominio local montado en debian o ubuntu, dichos servidor esta montado en una Jetson Nano 2GB con una sd de 64GB. La base de datos creara un backUp diario para evitar la perdida de información por la inestabilidad de las sd en las jetson.
+
+def titulo():
+    print ('*****************************************************************************')
+    print ('*****************************************************************************')
+    print (" ____                         _        ____      _     _  ___          ____  ")
+    print ("/ ___|  ___  _ __   ___  _ __| |_ ___ / ___|   _| |__ (_)/ _ \  __   _|___ \ ")
+    print ("\___ \ / _ \| '_ \ / _ \| '__| __/ _ \ |  | | | | '_ \| | | | | \ \ / / __) |")
+    print (" ___) | (_) | |_) | (_) | |  | ||  __/ |__| |_| | |_) | | |_| |  \ V / / __/ ")
+    print ("|____/ \___/| .__/ \___/|_|   \__\___|\____\__,_|_.__/|_|\__\_\   \_(_)_____|")
+    print ("            |_|                                                              ")
+    print ('*****************************************************************************')
+    print ('*****************************************************************************')
